9098f5222d0c990d317bf391d1d280f39be8c5a8
James Halliday <mail@substack.net>
2014-05-04 05:13:50 -0700

    set the distance and center in the example

--- example/browser.js
+++ example/browser.js
@@ -11,6 +11,8 @@ window.addEventListener('keydown', function (ev) {
 var mesh;
 shell.on('viewer-init', function () {
     mesh = shell.createMesh(require('bunny'));
+    shell.camera.distance = 10;
+    shell.camera.center = [ 0, 5, 0 ];
 });
 
 shell.on('gl-render', function () {
